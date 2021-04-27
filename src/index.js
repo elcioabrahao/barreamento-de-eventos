8924bcd6f6b294d093e1fbc5fd3f7b64cf1d73eb
Elcio Abrahao <elcio.abrahao@viavarejo.com.br>
2021-04-27 19:40:59 -0300

    correções

--- src/index.js
+++ src/index.js
@@ -1,22 +1,41 @@
 const express = require('express');
 //para enviar eventos para os demais microsserviços
+const eventos = []
 const axios = require('axios');
 const app = express();
 app.use(express.json());
 app.post("/eventos", async (req, res) => {
     const evento = req.body;
+    eventos.push(evento)
     //envia o evento para o microsserviço de lembretes
-    axios.post("http://localhost:4000/eventos", evento);
+    axios.post("http://localhost:4000/eventos", evento)
+        .catch((err) => {
+            console.log("err", err);
+        });
     //envia o evento para o microsserviço de observações
-    axios.post("http://localhost:5000/eventos", evento);
+    axios.post("http://localhost:5000/eventos", evento)
+        .catch((err) => {
+            console.log("err", err);
+        });
     //envia o evento para o microsserviço de consulta
-    axios.post("http://localhost:6000/eventos", evento);
+    axios.post("http://localhost:6000/eventos", evento)
+        .catch((err) => {
+            console.log("err", err);
+        });
     //envia o evento para o microsserviço de classificação
-    axios.post("http://localhost:7000/eventos", evento);
+    axios.post("http://localhost:7000/eventos", evento)
+        .catch((err) => {
+            console.log("err", err);
+        });
     res.status(200).send({
         msg: "ok"
     });
 });
+
+app.get('/eventos', (req, res) => {
+    res.send(eventos)
+})
+
 app.listen(10000, () => {
     console.log('Barramento de eventos. Porta 10000.')
 })
